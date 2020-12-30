3827d2c609ac33c1aad8704c6100b15d5d2d8e82
adrian <rivendell@protonmail.com>
2020-12-30 22:32:30 +0100

    kafelki

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+// import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Layout, Menu, Breadcrumb } from "antd";
@@ -15,80 +16,100 @@ import {
 import CurrencyConverter from "./CurrencyConverter";
 import Ert from "./Ert";
 
-
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 function App() {
   const [collapsed, setCollapsed] = useState(false);
-  const [value, onChange] = useState(new Date());
+  const [ert, setErt] = useState("", []);
+  // const [value, onChange] = useState(new Date());
+
+  useEffect(() => {
+    console.log("uruchomienie useEffect");
+    if (ert === "") {
+      console.log("uruchomienie if");
+      const url = `http://api.nbp.pl/api/exchangerates/tables/A/?format=json`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setErt(data);
+        })
+        .catch((e) => console.log("Connection error", e));
+    }
+  });
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={() => setCollapsed((prev) => !prev)}
-      >
-        <div className="logo">
-          <p className="ert">
-            <BarChartOutlined style={{ fontSize: "60px" }} />
-            <span>ert</span>
-          </p>
-        </div>
-        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          <Menu.Item key="1" icon={<StockOutlined />}>
-            Kursy walut
-          </Menu.Item>
-          <Menu.Item
-            key="2"
-            icon={<BankOutlined style={{ fontSize: "20px" }} />}
-          >
-            Kalkulator walutowy
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            icon={<DollarOutlined style={{ fontSize: "20px" }} />}
-            title="Waluty"
-          >
-            <Menu.Item key="3">EUR</Menu.Item>
-            <Menu.Item key="4">USD</Menu.Item>
-            <Menu.Item key="5">GBP</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
-            Files
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: "0 16px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, minHeight: 360 }}
-          >
-            <Calendar
-              onChange={onChange}
-              value={value}
-              onClickDay={(value, event) => console.log("Clicked day: ", value)}
-            />
-            <CurrencyConverter />
-            <Ert />
+    <Router basename={process.env.PUBLIC_URL}>
+      {console.log("render App")}
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={() => setCollapsed((prev) => !prev)}
+        >
+          <div className="logo">
+            <NavLink to="/">
+              <p className="ert">
+                <BarChartOutlined style={{ fontSize: "60px" }} />
+                <span>ert</span>
+              </p>
+            </NavLink>
           </div>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          ERT - Exchange Rate Table / Ant Design ©2020
-        </Footer>
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" icon={<StockOutlined />}>
+              <NavLink exact to="/">
+                Kursy walut
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item
+              key="2"
+              icon={<BankOutlined style={{ fontSize: "20px" }} />}
+            >
+              <NavLink to="/CurrencyConverter">Kalkulator walutowy</NavLink>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              icon={<DollarOutlined style={{ fontSize: "20px" }} />}
+              title="Waluty"
+            >
+              <Menu.Item key="3">EUR</Menu.Item>
+              <Menu.Item key="4">USD</Menu.Item>
+              <Menu.Item key="5">GBP</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item
+              key="9"
+              icon={<FileOutlined />}
+              onClick={() => console.log(ert)}
+            >
+              Files
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Route exact path="/" render={() => <Ert ert={ert} />} />
+              <Route path="/CurrencyConverter" component={CurrencyConverter} />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            ERT - Exchange Rate Table / Ant Design ©2020
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 }
 
--- src/components/Ert.js
+++ src/components/Ert.js
@@ -1,8 +1,122 @@
 import React from "react";
 import "./Ert.css";
 
-function Ert() {
-  return <div>Ert</div>;
+function Ert({ ert }) {
+  return (
+    <>
+      {console.log("render ert")}
+      <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,900"
+        rel="stylesheet"
+      />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-1" onClick={() => console.log(ert)}>
+              <h3>
+                {ert[0].rates[1].code} {ert[0].rates[1].mid}
+              </h3>
+              <p>{ert[0].rates[1].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-2">
+              <h3>
+                {ert[0].rates[7].code} {ert[0].rates[7].mid}
+              </h3>
+              <p>{ert[0].rates[7].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-3">
+              <h3>
+                {ert[0].rates[10].code} {ert[0].rates[10].mid}
+              </h3>
+              <p>{ert[0].rates[10].currency}</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-1" onClick={() => console.log(ert)}>
+              <h3>
+                {ert[0].rates[9].code} {ert[0].rates[9].mid}
+              </h3>
+              <p>{ert[0].rates[9].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-2">
+              <h3>
+                {ert[0].rates[13].code} {ert[0].rates[13].mid}
+              </h3>
+              <p>{ert[0].rates[13].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-3">
+              <h3>
+                {ert[0].rates[17].code} {ert[0].rates[17].mid}
+              </h3>
+              <p>{ert[0].rates[17].currency}</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-1" onClick={() => console.log(ert)}>
+              <h3>
+                {ert[0].rates[11].code} {ert[0].rates[11].mid}
+              </h3>
+              <p>{ert[0].rates[11].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-2">
+              <h3>
+                {ert[0].rates[16].code} {ert[0].rates[16].mid}
+              </h3>
+              <p>{ert[0].rates[16].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-3">
+              <h3>
+                {ert[0].rates[12].code} {ert[0].rates[12].mid}
+              </h3>
+              <p>{ert[0].rates[12].currency}</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-1" onClick={() => console.log(ert)}>
+              <h3>
+                {ert[0].rates[14].code} {ert[0].rates[14].mid}
+              </h3>
+              <p>{ert[0].rates[14].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-2">
+              <h3>
+                {ert[0].rates[18].code} {ert[0].rates[18].mid}
+              </h3>
+              <p>{ert[0].rates[18].currency}</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-3">
+              <h3>
+                {ert[0].rates[22].code} {ert[0].rates[22].mid}
+              </h3>
+              <p>{ert[0].rates[22].currency}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Ert;
